9183302029825d90eaf8d549546b5dd141cab484
Shrey Bosamia <78290845+ShreyBosamia@users.noreply.github.com>
2025-01-29 11:21:07 -0800

    Create database.js
    Added the connection to the database. Using .env to hide sensitive information, therefore the sensitive information will be in the .env file at the root directory, which we can hide with .gitignore

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+// Create a connection pool to manage multiple connections efficiently 
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+//Export the database connection so other files can use it
+module.exports = db;
