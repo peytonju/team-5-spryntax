e4d96b933e76bba6ddb36367394371064565c0a5
peytonju <peytonju@oregonstate.edu>
2025-02-02 23:34:20 -0800

    general requirements of the site
    lacks any form of visuals or being able to traverse between pages, but
    the support exists for you to be able to type the URL directly

--- website/spryntax.js
+++ website/spryntax.js
@@ -1,5 +1,3 @@
-console.log("asdf");
-
 const PORT = 8080;
 const express = require('express');
 const exphbs  = require('express-handlebars');
@@ -15,16 +13,47 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 
-/* queries to "/" just render the main page */
-app.get('/', (request, response) => {
-    console.log("user connected to /");
-    response.status(200).render("pages/mainPage");
+app.get("/", (request, response) => {
+    response.status(200).render("pages/main");
 });
 
-/* "/code" redirects to the code page */
-app.get('/code', (request, response) => {
-    response.status(200).render("pages/codePage")
+app.get("/leaderboards", (request, response) => {
+    response.status(200).render("pages/leaderboard_global");
+});
+
+app.get("/login", (request, response) => {
+    response.status(200).render("pages/login");
+});
+
+app.get("/signup", (request, response) => {
+    response.status(200).render("pages/signup");
+});
+
+app.get("/settings", (request, response) => {
+    response.status(200).render("pages/settings");
+});
+
+
+/*****************************************LEVELS******************************************************/
+app.get("/level_select", (request, response) => {
+    response.status(200).render("pages/level_select");
 })
+
+app.get('/level_select/:level_name', (request, response) => {
+    const level_name = request.params["level_name"].toLowerCase();
+    response.status(200).render("pages/level_desc");
+})
+
+app.get('/level_select/:level_name/play', (request, response) => {
+    const level_name = request.params["level_name"].toLowerCase();
+    response.status(200).render("pages/level_play");
+})
+
+app.get('/level_select/:level_name/end', (request, response) => {
+    const level_name = request.params["level_name"].toLowerCase();
+    response.status(200).render("pages/level_end");
+})
+/****************************************************************************************************/
 
 
 /* makes the express server listen on the specified port */
