bc6dd1ab45b98a7165e3cce390dd996c5c4ce435
peytonju <peytonju@oregonstate.edu>
2025-02-11 19:31:22 -0800

    convert spaces to tabs

--- website_static_build_tools/raw_alg_builder/algs-py/dlink.py
+++ website_static_build_tools/raw_alg_builder/algs-py/dlink.py
@@ -1,30 +1,30 @@
 # Python Program for traversal of a doubly linked list
 class Node:
-    def __init__(self, data):
-        # Initialize a new node with data, previous, and next pointers
-        self.data = data
-        self.next = None
-        self.prev = None
+	def __init__(self, data):
+		# Initialize a new node with data, previous, and next pointers
+		self.data = data
+		self.next = None
+		self.prev = None
 
 
 def traverse(head):
-    # Traverse the doubly linked list and print its elements
-    current = head
-    while current:
-      # Print current node's data
-        print(current.data, end=" <-> ")
-        # Move to the next node
-        current = current.next
-    print("None")
+	# Traverse the doubly linked list and print its elements
+	current = head
+	while current:
+	  # Print current node's data
+		print(current.data, end=" <-> ")
+		# Move to the next node
+		current = current.next
+	print("None")
 
 
 def insert_at_beginning(head, data):
-    # Insert a new node at the beginning of the doubly linked list
-    new_node = Node(data)
-    new_node.next = head
-    if head:
-        head.prev = new_node
-    return new_node
+	# Insert a new node at the beginning of the doubly linked list
+	new_node = Node(data)
+	new_node.next = head
+	if head:
+		head.prev = new_node
+	return new_node
 
 
 # Driver Code
--- website_static_build_tools/raw_alg_builder/algs-py/queue.py
+++ website_static_build_tools/raw_alg_builder/algs-py/queue.py
@@ -1,64 +1,64 @@
 class Queue:
-    def __init__(self, q_size):
-        self.arr = [0] * q_size
-        self.size = 0
-        self.capacity = q_size
-        self.front = 0
-    
-    # Function to add an element
-    # to queue.
-    def enqueue(self, x):
-        
-        # If queue is full
-        if self.size == self.capacity:
-            return
-        
-        self.arr[self.size] = x
-        
-        # Increment queue size.
-        self.size += 1
-    
-    # Function to pop front 
-    # element from queue.
-    def dequeue(self):
-        
-        # If queue is empty 
-        if self.size == 0:
-            return
-        
-        # Shift all the elements 
-        # to the left.
-        for i in range(1, self.size):
-            self.arr[i-1] = self.arr[i]
-        
-        # decrement queue size 
-        self.size -= 1
-    
-    # Function which returns 
-    # the front element.
-    def getFront(self):
-        
-        # If queue is empty
-        if self.size == 0:
-            return -1
-        
-        return self.arr[self.front]
-    
-    # Function which prints
-    # the elements of array.
-    def display(self):
-        
-        for i in range(self.front, self.size):
-            print(self.arr[i], end=" ")
-        print()
+	def __init__(self, q_size):
+		self.arr = [0] * q_size
+		self.size = 0
+		self.capacity = q_size
+		self.front = 0
+	
+	# Function to add an element
+	# to queue.
+	def enqueue(self, x):
+		
+		# If queue is full
+		if self.size == self.capacity:
+			return
+		
+		self.arr[self.size] = x
+		
+		# Increment queue size.
+		self.size += 1
+	
+	# Function to pop front 
+	# element from queue.
+	def dequeue(self):
+		
+		# If queue is empty 
+		if self.size == 0:
+			return
+		
+		# Shift all the elements 
+		# to the left.
+		for i in range(1, self.size):
+			self.arr[i-1] = self.arr[i]
+		
+		# decrement queue size 
+		self.size -= 1
+	
+	# Function which returns 
+	# the front element.
+	def getFront(self):
+		
+		# If queue is empty
+		if self.size == 0:
+			return -1
+		
+		return self.arr[self.front]
+	
+	# Function which prints
+	# the elements of array.
+	def display(self):
+		
+		for i in range(self.front, self.size):
+			print(self.arr[i], end=" ")
+		print()
 
 if __name__ == "__main__":
-    q = Queue(4)
-    
-    q.enqueue(1)
-    q.enqueue(2)
-    q.enqueue(3)
-    print(q.getFront())
-    q.dequeue()
-    q.enqueue(4)
-    q.display()
+	q = Queue(4)
+	
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	print(q.getFront())
+	q.dequeue()
+	q.enqueue(4)
+	q.display()
