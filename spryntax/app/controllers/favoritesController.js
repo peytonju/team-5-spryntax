321ca27e614b4375168d20b0e4e5a25f245dc4e0
Shrey Bosamia <shreybosamia@outlook.com>
2025-02-26 17:48:15 -0800

    Fined tuned the favorite page to make the cards show the level diffuculty and type

--- spryntax/app/controllers/favoritesController.js
+++ spryntax/app/controllers/favoritesController.js
@@ -1,4 +1,12 @@
 const Favorites = require('../models/favorites');
+const fs = require("fs");
+const PATH_LEVEL_TAGS = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "levels.json");
+const PATH_LEVEL_TO_CLEAN = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "to_clean_names.json");
+const PATH_LEVEL_TO_NONCLEAN = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "to_nonclean_names.json");
+
+const JSON_LEVEL_TAGS = JSON.parse(fs.readFileSync(PATH_LEVEL_TAGS).toString());
+const LEVEL_NAME_TO_READABLE = JSON.parse(fs.readFileSync(PATH_LEVEL_TO_CLEAN).toString());
+const LEVEL_NAME_TO_NONREADABLE = JSON.parse(fs.readFileSync(PATH_LEVEL_TO_NONCLEAN).toString());
 
 const favoritesController = {
   get_favorite: (req, res) => {
@@ -8,7 +16,13 @@ const favoritesController = {
         res.status(500).send('Error fetching products');
         return;
       }
-      res.render('favorites', { favorites: favorites});
+        res.render('favorites', { 
+            favorites: favorites,
+            level_tags: JSON_LEVEL_TAGS,
+            nonreadable_to_readable: LEVEL_NAME_TO_READABLE,  // to_clean_names.json
+            readable_to_nonreadable: LEVEL_NAME_TO_NONREADABLE  // to_nonclean_names.json
+        });
+    
     });
   },
 
