e9329dcac1d05e617ff6040a7882737fad693db0
Shrey Bosamia <shreybosamia@outlook.com>
2025-02-26 18:44:12 -0800

    Made it so you can delete favorites. Also changed some of the routing to enable proper permissions

--- spryntax/app/controllers/favoritesController.js
+++ spryntax/app/controllers/favoritesController.js
@@ -1,44 +1,39 @@
-const Favorites = require('../models/favorites');
-const fs = require("fs");
-const PATH_LEVEL_TAGS = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "levels.json");
-const PATH_LEVEL_TO_CLEAN = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "to_clean_names.json");
-const PATH_LEVEL_TO_NONCLEAN = require('path').join(__dirname, "../../website_static_build_tools/alg_extras", "to_nonclean_names.json");
+const fs = require('fs');
+const PATH = require('path');
 
-const JSON_LEVEL_TAGS = JSON.parse(fs.readFileSync(PATH_LEVEL_TAGS).toString());
-const LEVEL_NAME_TO_READABLE = JSON.parse(fs.readFileSync(PATH_LEVEL_TO_CLEAN).toString());
-const LEVEL_NAME_TO_NONREADABLE = JSON.parse(fs.readFileSync(PATH_LEVEL_TO_NONCLEAN).toString());
+const JSON_LEVEL_TAGS = JSON.parse(fs.readFileSync(PATH.join(__dirname, "../../website_static_build_tools/alg_extras/levels.json")).toString());
+const LEVEL_NAME_TO_READABLE = JSON.parse(fs.readFileSync(PATH.join(__dirname, "../../website_static_build_tools/alg_extras/to_clean_names.json")).toString());
+const LEVEL_NAME_TO_NONREADABLE = JSON.parse(fs.readFileSync(PATH.join(__dirname, "../../website_static_build_tools/alg_extras/to_nonclean_names.json")).toString());
+const Favorites = require('../models/favorites');
 
 const favoritesController = {
   get_favorite: (req, res) => {
     Favorites.getAll((err, favorites) => {
       if (err) {
-        console.error('Error fetching products:', err);
-        res.status(500).send('Error fetching products');
+        console.error('Error fetching favorites:', err);
+        res.status(500).send('Error fetching favorites');
         return;
       }
-        res.render('favorites', { 
-            favorites: favorites,
-            level_tags: JSON_LEVEL_TAGS,
-            nonreadable_to_readable: LEVEL_NAME_TO_READABLE,  // to_clean_names.json
-            readable_to_nonreadable: LEVEL_NAME_TO_NONREADABLE  // to_nonclean_names.json
-        });
-    
+      res.render('favorites', { 
+        favorites: favorites,
+        level_tags: JSON_LEVEL_TAGS,
+        nonreadable_to_readable: LEVEL_NAME_TO_READABLE,
+        readable_to_nonreadable: LEVEL_NAME_TO_NONREADABLE
+      });
     });
   },
 
   addFavorite: (req, res) => {
     if (!req.session.user_id) {
-      console.error('User not logged in');
       return res.status(401).json({ error: "User not logged in" });
     }
     const user_id = req.session.user_id;
     const algorithmName = req.body.algorithmName;
-  
+    
     if (!algorithmName) {
-      console.error('Algorithm name missing');
       return res.status(400).json({ error: "Algorithm name is required" });
     }
-  
+
     Favorites.addFavorite(user_id, algorithmName, (err, result) => {
       if (err) {
         console.error('Error adding favorite:', err);
@@ -46,9 +41,27 @@ const favoritesController = {
       }
       res.json({ success: true });
     });
+  },
+
+  removeFavorite: (req, res) => {
+    if (!req.session.user_id) {
+      return res.status(401).json({ error: "User not logged in" });
+    }
+    const user_id = req.session.user_id;
+    const algorithmName = req.body.algorithmName;
+    
+    if (!algorithmName) {
+      return res.status(400).json({ error: "Algorithm name is required" });
+    }
+    
+    Favorites.removeFavorite(user_id, algorithmName, (err, result) => {
+      if (err) {
+        console.error('Error removing favorite:', err);
+        return res.status(500).json({ error: "Database error" });
+      }
+      res.json({ success: true });
+    });
   }
-  
-  
 };
 
 module.exports = favoritesController;
--- spryntax/app/models/favorites.js
+++ spryntax/app/models/favorites.js
@@ -1,14 +1,17 @@
 const db = require('../../config/database');
 
 const Favorites = {
-    getAll: (callback) => {
-      db.query('SELECT * FROM favorites', callback);
-    },
-
-    addFavorite: (user_id, algorithmName, callback) => {
-        const query = 'INSERT INTO favorites (user_id, algorithm_name) VALUES (?, ?)';
-        db.query(query, [user_id, algorithmName], callback);
-    }
-  };
+  getAll: (callback) => {
+    db.query('SELECT * FROM favorites', callback);
+  },
+  addFavorite: (user_id, algorithmName, callback) => {
+    const query = 'INSERT INTO favorites (user_id, algorithm_name) VALUES (?, ?)';
+    db.query(query, [user_id, algorithmName], callback);
+  },
+  removeFavorite: (user_id, algorithmName, callback) => {
+    const query = 'DELETE FROM favorites WHERE user_id = ? AND algorithm_name = ?';
+    db.query(query, [user_id, algorithmName], callback);
+  }
+};
 
 module.exports = Favorites;
